043827110b2d28081a558b4cf6fef3cb13968671
Alexander Nilsson <alexvnilsson@outlook.com>
2018-06-21 20:53:40 +0200

    Updated root App-component.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import AppHeader from './App/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AppHeader from 'Core/UI/Header';
 
-class App extends Component {
+import Home from 'Pages/Home';
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <AppHeader></AppHeader>
-
-        <div className="content">
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
       </div>
     );
   }
 }
-
-export default App;
