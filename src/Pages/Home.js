f996d60274c317f39ee94debbb84bbc0c6ee74d0
Alexander Nilsson <alexvnilsson@outlook.com>
2018-06-23 12:36:04 +0200

    Minor content changes to app "page"-components.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import posed, { PoseGroup } from 'react-pose';
+
+import Button from 'Core/UI/Components/Button';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="content">
-        <h1>Who is Alexander?</h1>
+        <h1>Lorem ipsum</h1>
 
         <p>
-          Hello! I am a web developer, pure and simple. I work with NodeJs, ExpressJs, ReactJs, Angular <em>(not AngularJs)</em>.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in lacus et dolor auctor vulputate. In at faucibus neque. Curabitur quis ornare lectus. Ut placerat tincidunt imperdiet. Nulla dictum pellentesque mi et pulvinar. Ut fermentum enim in mauris mattis feugiat. Sed auctor odio nec ex lobortis, aliquet sodales leo semper. Nam eget vehicula mauris.
+        </p>
+
+        <p className="text-align-center">
+          <Link to="/showcase">
+            <Button>Portfolio showcase</Button>
+          </Link>
         </p>
       </div>
     );
