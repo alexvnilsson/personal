082353310d92276e9a65c52ba39654c274a909f5
Alexander Nilsson <alexvnilsson@outlook.com>
2018-07-04 14:45:14 +0200

    Updated Home.js-screen.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,21 +4,68 @@ import posed, { PoseGroup } from 'react-pose';
 
 import Button from 'Core/UI/Components/Button';
 
+import SeamlessItem from 'Core/UI/Components/Itemization/SeamlessItem';
+
 export default class Home extends Component {
   render() {
     return (
       <div className="content">
-        <h1>Lorem ipsum</h1>
+        <h1>Alexander Nilsson ‐ en webbutvecklare</h1>
 
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in lacus et dolor auctor vulputate. In at faucibus neque. Curabitur quis ornare lectus. Ut placerat tincidunt imperdiet. Nulla dictum pellentesque mi et pulvinar. Ut fermentum enim in mauris mattis feugiat. Sed auctor odio nec ex lobortis, aliquet sodales leo semper. Nam eget vehicula mauris.
+          Jag har under större delen av 2010-talet utbildat mig & arbetat med webbdesign och -teknik.
         </p>
 
-        <p className="text-align-center">
+        <p className="text-align-center margin-y-3">
           <Link to="/showcase">
-            <Button>Portfolio showcase</Button>
+            <Button>Portfölj</Button>
           </Link>
+
+          <br />
+
+          <span className="inline-block margin-top-1 text-darken">eller läs mer nedan</span>
         </p>
+
+        <div className="margin-top-4"></div>
+
+        <div>
+          <h2>Arbetslivserharenhet</h2>
+
+          <SeamlessItem heading="Egenföretagare" subheading="2016-2017">
+            Drev egen firma fr.o.m. sena 2015 under mitt egna namn, hösten 2016 grundade jag (ej ensam) ett aktiebolag med namnet <span className="text-muted" title="Detta är ännu inte en länk.">NC NetCommando AB</span>, som gick i konkurs ett år senare (september, 2017).
+
+            <p>
+              För mig blev det tydligt väldigt fort att jag inte var "business"-begåvad, utan jag är tekniskt inriktad.
+            </p>
+          </SeamlessItem>
+
+          <SeamlessItem heading="Webbansvarig/-utvecklare, ProPublic AB" subheading="2013-2016">
+            Drev webbutveckling och -drift åt denna konsultfirma som startade sin verksamhet vid årsskiftet 2013-2014. Här hade jag möjlighet att under flexibla omständigheter utveckla mina komptenser inom området.
+          </SeamlessItem>
+        </div>
+
+        <div>
+          <h2>Utbildning <span className="text-muted text-size-smaller">(kronologisk ordning)</span></h2>
+
+          <SeamlessItem heading="Grillska gymnasium, Västerås" subheading="2013-2014">
+            Datakunskaplig inriktning, webbutveckling och datorkunskap.
+          </SeamlessItem>
+
+          <SeamlessItem heading="IT-gymnasiet, Skövde" subheading="2014">
+            Datakunskaplig inriktning, webbutveckling och datorkunskap (forts.)
+          </SeamlessItem>
+
+          <SeamlessItem heading="Vuxenutbildningscentrum, Skövde" subheading="2014-2015">
+            Skolan gick åt helvete, försökte studera på distans.
+          </SeamlessItem>
+
+          <SeamlessItem heading="Vuxenutbildningscentrum, Västerås" subheading="2015-2016">
+            Distansstudier i Skövde gick åt helvete (märker du ett mönster), flyttade hem till Västerås, försökte med distansstudier.
+          </SeamlessItem>
+        </div>
+
+        <p></p>
+
       </div>
     );
   }
