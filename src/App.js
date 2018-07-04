a64e305bffc5e2e18c4760dcb2b68bd66ffbe6f4
Alexander Nilsson <alexvnilsson@outlook.com>
2018-07-04 15:28:16 +0200

    Added basename to react-router.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
     return (
       <div>
         <AppHeader></AppHeader>
-        <Router>
+        <Router basename="/personal">
           <div>
             <Route exact path="/" component={Home} />
             <Route path="/showcase" component={Showcase} />
