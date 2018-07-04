6fca1537c55a1858c1140b559fe4f9187c2462c6
Alexander Nilsson <alexvnilsson@outlook.com>
2018-07-04 15:15:29 +0200

    Removed unused deps.

--- src/Core/UI/Header.js
+++ src/Core/UI/Header.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import BrandLogo from 'Core/UI/BrandLogo';
-//import logo from 'brand-dark.svg';
 
 export default class AppHeader extends Component {
-    render() {
-        return (
-            <div className="app-header-container">
-                <header className="app-header content">
-                    <div className="header-brand">
-                      <BrandLogo className="brand-logo"></BrandLogo>
-                      {/* <img src={logo} className="brand-logo" alt="logo" /> */}
-                      <h1 className="brand-name">Alex V. Nilsson</h1>
-                    </div>
+  render() {
+    return (
+      <div className="app-header-container">
+        <header className="app-header content">
+          <div className="header-brand">
+            <BrandLogo className="brand-logo"></BrandLogo>
+            <h1 className="brand-name">Alex V. Nilsson</h1>
+          </div>
 
-                    <ul className="header-right-nav">
-                        <li><a href="#">Legal</a></li>
-                        <li><a href="#">Press</a></li>
-                    </ul>
-                </header>
-            </div>
-        );
-    }
+          <ul className="header-right-nav">
+            <li><a href="#">Legal</a></li>
+            <li><a href="#">Press</a></li>
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import posed, { PoseGroup } from 'react-pose';
 
 import Button from 'Core/UI/Components/Button';
 
