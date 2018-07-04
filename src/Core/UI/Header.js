73ac73c6cf3cbdb58bb658b0867830064a08b92d
Alexander Nilsson <alexvnilsson@outlook.com>
2018-07-04 16:45:34 +0200

    Updated Header.js
    - Made header-brand into a link to homepage.

--- src/Core/UI/Header.js
+++ src/Core/UI/Header.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import BrandLogo from 'Core/UI/BrandLogo';
 
+import { Link } from 'react-router-dom';
+
 export default class AppHeader extends Component {
   render() {
     return (
       <div className="app-header-container">
         <header className="app-header content">
-          <div className="header-brand">
+          <Link to="/" className="header-brand">
             <BrandLogo className="brand-logo"></BrandLogo>
             <h1 className="brand-name">Alex V. Nilsson</h1>
-          </div>
+          </Link>
 
           <ul className="header-right-nav">
-            <li><a href="https://github.com/alexvnilsson" target="_blank"><img src="/assets/github-logo.svg" width="25px" /></a></li>
-            <li><a href="https://www.linkedin.com/in/alexvnilsson/" target="_blank"><img src="/assets/linkedin-logo.svg" width="25px" /></a></li>
+            <li><a href="https://github.com/alexvnilsson" target="_blank"><img src={process.env.PUBLIC_URL + "/assets/github-logo.svg"} width="25px" /></a></li>
+            <li><a href="https://www.linkedin.com/in/alexvnilsson/" target="_blank"><img src={process.env.PUBLIC_URL + "/assets/linkedin-logo.svg"} width="25px" /></a></li>
           </ul>
         </header>
       </div>
