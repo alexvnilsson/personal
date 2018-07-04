2fe065f66970ad8de568da8feb8ed2c8755ada15
Alexander Nilsson <alexvnilsson@outlook.com>
2018-07-04 16:45:13 +0200

    Updated App.js
    - Re-located AppHeader-component to inside of Router-comp.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AppHeader from 'Core/UI/Header';
 
 import Home from 'Pages/Home';
-import Showcase from 'Pages/Showcase';
+import Portfolio from 'Pages/Portfolio';
 
 // @TODO Implement https://github.com/maisano/react-router-transition
 
@@ -12,11 +12,12 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <AppHeader></AppHeader>
         <Router basename="/personal">
           <div>
+          <AppHeader></AppHeader>
+
             <Route exact path="/" component={Home} />
-            <Route path="/showcase" component={Showcase} />
+            <Route path="/portfolio" component={Portfolio} />
           </div>
         </Router>
       </div>
