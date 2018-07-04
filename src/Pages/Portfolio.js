cd908728b4c5d67c270bf369c70dea01993cc6f8
Alexander Nilsson <alexvnilsson@outlook.com>
2018-07-04 16:46:04 +0200

    Updated Showcase.js
    - Renamed Showcase.js to Portfolio.js
    - Added some placeholder content to Portfolio.js

---
+++ src/Pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Portfolio extends Component {
+  render() {
+    return (
+      <div className="content">
+        <h1>Oops!</h1>
+
+        <h2>Har inte hunnit skapa innehållet på denna sida ännu.</h2>
+      </div>
+    );
+  }
+}
