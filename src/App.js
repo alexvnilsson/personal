da3977157508cdd78fde3117977a9e164a824f20
Alexander Nilsson <alexvnilsson@outlook.com>
2018-07-06 19:53:11 +0200

    Implemented basic, dynamic app-wide color theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import { store } from 'Core/UI/Store';
+
 import AppHeader from 'Core/UI/Header';
 
 import Home from 'Pages/Home';
@@ -8,19 +10,44 @@ import Portfolio from 'Pages/Portfolio';
 
 // @TODO Implement https://github.com/maisano/react-router-transition
 
-export default class App extends Component {
+export default class App extends Component {  
+  constructor() {
+    super()
+
+    this.colorTheme = '';
+
+    
+  }
+
+  componentWillMount() {
+    this.uiStoreListener = store.subscribe(() => {
+      const state = store.getState();
+
+      console.log(state);    
+
+      if (this.colorTheme !== state.colorTheme) {
+        this.colorTheme = state.colorTheme;
+        this.forceUpdate();
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    if (typeof this.uiStoreListener !== 'undefined') {
+      this.uiStoreListener();
+    }
+  }
+  
   render() {
     return (
-      <div>
-        <Router basename="/personal">
-          <div>
+      <Router basename="/personal">
+        <div className={"content-container " + this.colorTheme}>
           <AppHeader></AppHeader>
 
-            <Route exact path="/" component={Home} />
-            <Route path="/portfolio" component={Portfolio} />
-          </div>
-        </Router>
-      </div>
+          <Route exact path="/" component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+        </div>
+      </Router>
     );
   }
 }
