d9a0bb5627a35842ed63cdd2ff154a34f6d6b55e
Alexander Nilsson <alexvnilsson@outlook.com>
2018-07-09 15:14:05 +0200

    Updated Portfolio.js
    - Added some initial content (ComdoCRM + icons).

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 import { store, actions } from 'Core/UI/Store';
 
+import ComdoCRM from './Portfolio/ComdoCRM';
+
 export default class Portfolio extends Component {
+  get Subpages() {
+    return {
+      ComdoCRM: ComdoCRM
+    }
+  }
+
   componentWillMount() {
     store.dispatch(actions.setColorTheme('light'));
   }
@@ -10,10 +18,25 @@ export default class Portfolio extends Component {
   render() {
    return (
       <div className="back-light">
-        <div className="content">
-          <h1>Oops!</h1>
+        <div className="content text-dark">
+          <h1>Min portfölj</h1>
 
-          <h2>Har inte hunnit skapa innehållet på denna sida ännu.</h2>
+          <p>Här samlar jag mina tidigare projekt som kan vara av intresse.</p>
+
+          <div className="margin-top-2 text-align-center">
+            <div>
+              <img src={process.env.PUBLIC_URL + "/assets/3p/comdocrm.png"} alt="ComdoCRM logo" width="48px" />
+              <h4 className="margin-y-0">ComdoCRM</h4>
+
+              <p className="margin-y-tiny text-align-center">
+                <a className="button small no-shadow" href="https://github.com/alexvnilsson/comdocrm-app" target="_blank" rel="noopener noreferrer">Source code | GitHub</a>
+              </p>
+
+              <p className="margin-top-2">
+                En webbapplikation som under NetCommando skulle säljas som ett verktyg för kundrelationshantering och säljstöd.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
