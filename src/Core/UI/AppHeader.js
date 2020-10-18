74fea2e78dd939ef2c0cd65a9ef56f7898e51df3
alexvnilsson <alexvnilsson@outlook.com>
2020-10-18 22:54:21 +0200

    Update header and brand.

--- src/Core/UI/AppHeader.js
+++ src/Core/UI/AppHeader.js
@@ -1,72 +1,35 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import classNames from "classnames";
 
-import { store } from "Core/UI/Store";
-
-import BrandLogo from "Core/UI/BrandLogo";
-import Icon from "Core/UI/Components/Icon";
+import BrandLogo from "Assets/Brand/BrandDark.png";
 
 export default class AppHeader extends Component {
-  componentWillMount() {
-    this.uiStoreListener = store.subscribe(() => {
-      this.forceUpdate();
-    });
-  }
-
-  componentWillUnmount() {
-    if (typeof this.uiStoreListener !== "undefined") {
-      this.uiStoreListener();
-    }
-  }
-
   render() {
-    const { colorTheme } = store.getState();
-
     const navItems = [
       {
-        href: "https://github.com/alexvnilsson",
-        target: "_blank",
-        icon: "github"
+        href: "/portfolio",
+        label: "Portfölj",
       },
       {
-        href: "https://www.linkedin.com/in/alexvnilsson",
-        target: "_blank",
-        icon: "linkedin"
-      }
+        href: "/toolbox",
+        label: "Verktygslåda",
+      },
     ];
 
     return (
-      <div
-        className={classNames(
-          "app-header-container",
-          colorTheme.header || "light"
-        )}
-      >
-        <header className="app-header content">
-          <Link to="/" className="header-brand">
-            <BrandLogo className="brand-logo" />
-            <h1 className="brand-name">Alex V. Nilsson</h1>
-          </Link>
+      <header className="app-header container">
+        <Link to="/" className="header-brand">
+          <img src={BrandLogo} alt="Logotyp" className="brand-logo-image" />
+        </Link>
 
-          <ul className="header-right-nav">
-            {navItems.map((item, index) => (
-              <li key={index} className="nav-item">
-                <a
-                  className="nav-link"
-                  href={item.href}
-                  target={item.target || "_blank"}
-                  rel="noreferrer noopener"
-                >
-                  <Icon className={classNames("nav-icon")} width="25px">
-                    {item.icon}
-                  </Icon>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </header>
-      </div>
+        <ul className="header-nav nav-right site-nav">
+          {navItems.map((item, index) => (
+            <li key={index} className="nav-item">
+              <Link to={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </header>
     );
   }
 }
