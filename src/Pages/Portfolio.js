c4e2f7046cf0382fa60059b8401b4d3c65220d73
alexvnilsson <alexvnilsson@outlook.com>
2020-10-18 23:03:24 +0200

    Update pages Portfolio, Toolbox.

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,76 +1,68 @@
 import React, { Component } from "react";
 
-import { store, actions } from "Core/UI/Store";
-
 export default class Portfolio extends Component {
-  componentWillMount() {
-    store.dispatch(actions.setColorTheme("light"));
-  }
-
   render() {
     return (
-      <div className="back-light">
-        <div className="content text-dark">
-          <h1>Min portfölj</h1>
+      <div className="container">
+        <h1 className="page-header">Min portfölj</h1>
 
-          <p>Här samlar jag mina tidigare projekt som kan vara av intresse.</p>
+        <p>Här samlar jag mina tidigare projekt som kan vara av intresse.</p>
 
-          <div class="margin-top-3" />
+        <div className="margin-top-3" />
 
-          <div className="margin-top-2 text-align-center">
-            <div>
-              <img
-                src={process.env.PUBLIC_URL + "/assets/favicon-96.png"}
-                alt="AlexVNilsson logo"
-                width="48px"
-              />
-              <h4 className="margin-y-0">alexvnilsson.github.io/personal</h4>
+        <div className="margin-top-2 text-align-center">
+          <div>
+            <img
+              src={process.env.PUBLIC_URL + "/assets/favicon-96.png"}
+              alt="AlexVNilsson logo"
+              width="48px"
+            />
+            <h4 className="margin-y-0">alexvnilsson.github.io/personal</h4>
 
-              <p className="margin-y-tiny text-align-center">
-                <a
-                  className="button small no-shadow"
-                  href="https://github.com/alexvnilsson/personal"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Källkod på GitHub
-                </a>
-              </p>
+            <p className="margin-y-tiny text-align-center">
+              <a
+                className="button small no-shadow"
+                href="https://github.com/alexvnilsson/personal"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Källkod på GitHub
+              </a>
+            </p>
 
-              <p className="margin-top-2">
-                Min personliga webbplats (som du besöker just nu) utvecklad i
-                React, för personlig marknadsföring och jobbansökningar.
-              </p>
-            </div>
+            <p className="margin-top-2">
+              Min personliga webbplats (som du besöker just nu) utvecklad i
+              React, för personlig marknadsföring och jobbansökningar.
+            </p>
           </div>
+        </div>
 
-          <div className="margin-top-2 text-align-center">
-            <div>
-              <img
-                src={process.env.PUBLIC_URL + "/assets/3p/comdocrm.png"}
-                alt="ComdoCRM logo"
-                width="48px"
-              />
-              <h4 className="margin-y-0">ComdoCRM</h4>
+        <div className="margin-top-2 text-align-center">
+          <div>
+            <img
+              src={process.env.PUBLIC_URL + "/assets/3p/comdocrm.png"}
+              alt="ComdoCRM logo"
+              width="48px"
+            />
+            <h4 className="margin-y-0">ComdoCRM</h4>
 
-              <p className="margin-y-tiny text-align-center">
-                <a
-                  className="button small no-shadow"
-                  href="https://github.com/alexvnilsson/comdocrm-app"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Källkod på GitHub
-                </a>
-              </p>
+            <p className="margin-y-tiny text-align-center">
+              <a
+                className="button small no-shadow"
+                href="https://github.com/alexvnilsson/comdocrm-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Källkod på GitHub
+              </a>
+            </p>
 
-              <p className="margin-top-2">
-                En webbapplikation som skulle säljas som ett verktyg för
-                kundrelationshantering och säljstöd, utvecklad under
-                NetCommando, av mig själv. Utvecklad i Angular (v5), back-end:an
-                utvecklades i ASP.Net Core (källkoden inte släppt).
-              </p>
-            </div>
+            <p className="margin-top-2">
+              En webbapplikation som skulle säljas som ett verktyg för
+              kundrelationshantering och säljstöd, utvecklad under NetCommando,
+              av mig själv. Utvecklad i Angular (v5), back-end:an utvecklades i
+              ASP.Net Core (källkoden inte släppt).
+            </p>
           </div>
         </div>
       </div>
--- src/Pages/Toolbox.js
+++ src/Pages/Toolbox.js
@@ -9,19 +9,12 @@ import { FadeInAndPanDown, FadeIn } from "Core/UI/Transitions";
 
 import { SpaceSeparator } from "Core/UI/Components/Separators";
 
-import { store, actions } from "Core/UI/Store";
-
 export default class Toolbox extends Component {
-  componentWillMount() {
-    store.dispatch(actions.setColorTheme({ body: "light", header: "light" }));
-  }
-
   render() {
     return (
-      <div className="page">
+      <div className="container">
         <FadeIn delay={100} duration={1000}>
           <div
-            className="container"
             style={{
               backgroundImage: "url(assets/parts/layers.png)",
               backgroundPosition: "85% top",
@@ -29,135 +22,125 @@ export default class Toolbox extends Component {
               backgroundSize: "288px",
             }}
           >
-            <div className="pre-content mb-4">
-              <h1 className="font-weight-normal">Min verktygslåda</h1>
+            <div className="mb-4">
+              <h1 className="page-header">Min verktygslåda</h1>
               <h6>
                 Detaljerad genomgång över teknologier jag har erfarenhet av i
                 produktionsmiljöer.
               </h6>
             </div>
 
-            <div className="content text-dark">
-              <TransitionGroup className="container">
-                <FadeInAndPanDown delay={200} duration={500}>
-                  <Deck pad bgColor="#324b96" color="#f5f5f5">
-                    <DeckTitle>Datalogik</DeckTitle>
+            <TransitionGroup className="container">
+              <FadeInAndPanDown delay={200} duration={500}>
+                <Deck pad bgColor="#324b96" color="#f5f5f5">
+                  <DeckTitle>Datalogik</DeckTitle>
 
-                    <DeckBody>
-                      <DeckGroup>
-                        <ListItem title="MySQL" icon="mysql">
-                          <Progress type="linear" value="80" />
+                  <DeckBody>
+                    <DeckGroup>
+                      <ListItem title="MySQL" icon="mysql">
+                        <Progress type="linear" value="80" />
 
-                          <p className="description">
-                            Erfarenhet av (Query SQL)?, MySQL Workbench
-                          </p>
-                        </ListItem>
+                        <p className="description">
+                          Erfarenhet av (Query SQL)?, MySQL Workbench
+                        </p>
+                      </ListItem>
 
-                        <ListItem title="SQL Server" icon="sqlserver">
-                          <Progress type="linear" value={50} />
+                      <ListItem title="SQL Server" icon="sqlserver">
+                        <Progress type="linear" value={50} />
 
-                          <p className="description">
-                            Erfarenhet av Transact-SQL, Migrations med EF
-                            <sup>1</sup> Core och EF 6+
-                            <br />
-                            <br />
-                            <sup>1</sup> <small>Entity Framework</small>
-                          </p>
-                        </ListItem>
+                        <p className="description">
+                          Erfarenhet av Transact-SQL, Migrations med EF
+                          <sup>1</sup> Core och EF 6+
+                          <br />
+                          <br />
+                          <sup>1</sup> <small>Entity Framework</small>
+                        </p>
+                      </ListItem>
 
-                        <ListItem title="MongoDB" icon="mongodb">
-                          <Progress type="linear" value="40" />
+                      <ListItem title="MongoDB" icon="mongodb">
+                        <Progress type="linear" value="40" />
 
-                          <p className="description">
-                            Erfarenhet av användning med NodeJS-baserade servrar
-                          </p>
-                        </ListItem>
-                      </DeckGroup>
-                    </DeckBody>
-                  </Deck>
-                </FadeInAndPanDown>
+                        <p className="description">
+                          Erfarenhet av användning med NodeJS-baserade servrar
+                        </p>
+                      </ListItem>
+                    </DeckGroup>
+                  </DeckBody>
+                </Deck>
+              </FadeInAndPanDown>
 
-                <SpaceSeparator />
+              <SpaceSeparator />
 
-                <FadeInAndPanDown delay={300} duration={500}>
-                  <Deck pad bgColor="#32e1c8" color="#2d2d2d">
-                    <DeckTitle>Affärslogik</DeckTitle>
+              <FadeInAndPanDown delay={300} duration={500}>
+                <Deck pad bgColor="#32e1c8" color="#2d2d2d">
+                  <DeckTitle>Affärslogik</DeckTitle>
 
-                    <DeckBody>
-                      <DeckGroup title="NodeJS">
-                        <ListItem title="ExpressJS" icon="expressjs">
-                          <Progress type="linear" value="75" />
+                  <DeckBody>
+                    <DeckGroup title="NodeJS">
+                      <ListItem title="ExpressJS" icon="expressjs">
+                        <Progress type="linear" value="75" />
 
-                          <p className="description">
-                            Beskrivning av teknologin
-                          </p>
-                        </ListItem>
+                        <p className="description">Beskrivning av teknologin</p>
+                      </ListItem>
 
-                        <ListItem title="SailsJS" icon="sailsjs">
-                          <Progress type="linear" value="40" />
+                      <ListItem title="SailsJS" icon="sailsjs">
+                        <Progress type="linear" value="40" />
 
-                          <p className="description">
-                            Beskrivning av teknologin
-                          </p>
-                        </ListItem>
-                      </DeckGroup>
+                        <p className="description">Beskrivning av teknologin</p>
+                      </ListItem>
+                    </DeckGroup>
 
-                      <DeckGroup title="Microsoft">
-                        <ListItem title="ASP.NET Core MVC" icon="netcore">
-                          <Progress type="linear" value="66" />
+                    <DeckGroup title="Microsoft">
+                      <ListItem title="ASP.NET Core MVC" icon="netcore">
+                        <Progress type="linear" value="66" />
 
-                          <p className="description">
-                            Beskrivning av teknologin
-                          </p>
-                        </ListItem>
-                      </DeckGroup>
-                    </DeckBody>
-                  </Deck>
-                </FadeInAndPanDown>
+                        <p className="description">Beskrivning av teknologin</p>
+                      </ListItem>
+                    </DeckGroup>
+                  </DeckBody>
+                </Deck>
+              </FadeInAndPanDown>
 
-                <SpaceSeparator />
+              <SpaceSeparator />
 
-                <FadeInAndPanDown delay={400} duration={500}>
-                  <Deck pad bgColor="rgb(255, 200, 50)" color="#2d2d2d">
-                    <DeckTitle>Presentationslager</DeckTitle>
+              <FadeInAndPanDown delay={400} duration={500}>
+                <Deck pad bgColor="rgb(255, 200, 50)" color="#2d2d2d">
+                  <DeckTitle>Presentationslager</DeckTitle>
 
-                    <DeckBody>
-                      <DeckGroup title="NodeJS">
-                        <ListItem title="Angular" icon="angular">
-                          <Progress type="linear" value="80" />
+                  <DeckBody>
+                    <DeckGroup title="NodeJS">
+                      <ListItem title="Angular" icon="angular">
+                        <Progress type="linear" value="80" />
 
-                          <p className="description">
-                            Utvecklat stor-skaliga affärssystem för ...
-                          </p>
-                        </ListItem>
+                        <p className="description">
+                          Utvecklat stor-skaliga affärssystem för ...
+                        </p>
+                      </ListItem>
 
-                        <ListItem title="React" icon="react">
-                          <Progress type="linear" value="66" />
+                      <ListItem title="React" icon="react">
+                        <Progress type="linear" value="66" />
 
-                          <p className="description" />
-                        </ListItem>
+                        <p className="description" />
+                      </ListItem>
 
-                        <ListItem title="VueJS" icon="vuejs">
-                          <Progress type="linear" value="75" />
+                      <ListItem title="VueJS" icon="vuejs">
+                        <Progress type="linear" value="75" />
 
-                          <p className="description" />
-                        </ListItem>
-                      </DeckGroup>
+                        <p className="description" />
+                      </ListItem>
+                    </DeckGroup>
 
-                      <DeckGroup title="Microsoft">
-                        <ListItem title="ASP.NET Core MVC" icon="netcore">
-                          <Progress type="linear" value="66" />
+                    <DeckGroup title="Microsoft">
+                      <ListItem title="ASP.NET Core MVC" icon="netcore">
+                        <Progress type="linear" value="66" />
 
-                          <p className="description">
-                            Beskrivning av teknologin
-                          </p>
-                        </ListItem>
-                      </DeckGroup>
-                    </DeckBody>
-                  </Deck>
-                </FadeInAndPanDown>
-              </TransitionGroup>
-            </div>
+                        <p className="description">Beskrivning av teknologin</p>
+                      </ListItem>
+                    </DeckGroup>
+                  </DeckBody>
+                </Deck>
+              </FadeInAndPanDown>
+            </TransitionGroup>
           </div>
         </FadeIn>
       </div>
